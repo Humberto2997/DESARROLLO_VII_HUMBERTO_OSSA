968ad35f2bcf2b9bec1861b3e2ec4af07c7c7f92
Humberto Ossa <titoossa29@gmail.com>
2024-08-23 12:50:49 -0500

    Agregada concatenacion de variables y constantes

--- TALLERES/TALLER_2/index.php
+++ TALLERES/TALLER_2/index.php
@@ -1,13 +1,21 @@
 <?php
 $nombre = "juan";
 $edad = 25;
-$altura = 1.75;
-$esEstudiante = true;
 
-echo "Nombre: $nombre<br>";
-echo "Edad: $edad<br>";
-echo "Altura: $altura<br>";
-echo "¿Es Estudiante? " . ($esEstudiante ? "Si" : "No");
+// concatenacion usando el operados.
+$presentacion1 = "Hola, mi nombre es " . $nombre . " y tengo " . $edad . " años.";
 
+// concatenacion dentreo de la comillas dobles
+$presentacion2 = "Hola, mi nombre es $nombre y tengo $edad años.";
+
+//Definicion de una constante
+define("SALUDO","¡Bienvenido!");
+
+//concatenacion con constante
+$mensaje = SALUDO. " " . $nombre;
+
+echo $presentacion1 . "<br>";
+echo $presentacion2 . "<br>";
+echo $mensaje . "<br>";
 ?>
 
